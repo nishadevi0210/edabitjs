d1405155fa31fbf14e6477b50388253e67cf6d9c
nishadevi0210 <nishadevi0210@gmail.com>
2020-03-13 16:49:11 +0100

    4 and 5 solutions completed

--- externalJS/javascriptAlgorithm.js
+++ externalJS/javascriptAlgorithm.js
@@ -1,8 +1,49 @@
 $(document).ready(function(){
-	console.log("hello");
 	$(document).on("click", ".soluDiv > button", function(){
-		var x = $(this).attr("id");
+		let x = $(this).attr("id");
 		$("#demo"+x).toggle();
-		console.log("here", "#demo"+x);
+
+		let result ="";
+		switch(x){
+			case "btn1":
+				result = getArea(5, 8);
+				break;
+			case "btn2":
+				let array = [1,2,3,4];
+				result = getFirstElement(array);
+				break;
+			case "btn3":
+				result = nextEdge(8, 10);
+				break;
+			case "btn4":
+				result = isSameNum(3, 3);
+				break;
+			case "btn5":
+				result = addUpTo(5);
+		}
+		$("#demo"+x).html(result);
 	});
+
+	function getArea(b, h){
+		return b*h/2 ;
+	}
+	function getFirstElement(a){
+		return a[0];
+	}
+	function nextEdge(a, b){
+		return a+b-1 ;
+	}
+	function isSameNum(a, b){
+		if (a==b){
+			return true;
+		}
+		return false;	
+	}
+	function addUpTo(n) {
+		let sum = 0;
+		for(i=1; i<=n; i++){
+			sum += i;
+		}
+		return sum;
+	}
 });
